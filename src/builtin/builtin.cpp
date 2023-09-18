b194b4c0a5a6f10bcded88f08c3b503b1a2082f5
nowcoder <1280883318@qq.com>
2023-09-18 11:09:59 +0800

    buildin cmd

--- src/builtin/builtin.cpp
+++ src/builtin/builtin.cpp
@@ -1,7 +1,10 @@
 #include "builtin.h"
 #include <iostream>
 #include <string>
-
+#include <stdlib.h>
+#include <unistd.h>
+#include <minsh/showp.h>
+std::map<std::string,int(*)(std::vector<std::string>)> builtin_cmd_tab;
 void buildin_cmd_init() {
     builtin_cmd_tab["echo"] = minsh_echo;
     //buildin_cmd_tab["cd"] = minsh_change_directory;
@@ -12,8 +15,13 @@ void buildin_cmd_init() {
 }
 
 int minsh_echo(std::vector<std::string> _args) {
-    for(auto s : _args) {
-        std::cout << s;
+    for(auto s = _args.begin(); s != _args.end(); s++) {
+        if(s != _args.end()){
+            std::cout << *s << " ";
+        }
+        else{
+            std::cout << *s <<std::endl;
+        }
     }
     std::cout << std::endl;
     return 0;
@@ -21,22 +29,57 @@ int minsh_echo(std::vector<std::string> _args) {
 
 int minsh_help(std::vector<std::string> _args);
 
-int minsh_change_directory(std::vector<std::string> _args);
+int minsh_change_directory(std::vector<std::string> _args){
+    if(_args.size() > 2){
+        std::cout << "too many arguments" << std::endl;
+        return 0;
+    }
+    if(_args.size() == 1){
+        //cd
+        int chdir_ret = chdir(home_dir.c_str());
+        return chdir_ret;
+    }
+    else{
+        //cd /home/linux
+        int chdir_ret = chdir(_args.at(1).c_str());
+        return chdir_ret;
+    }
+    // if(_args.at(0) == "cd")
+    // {
+    //     int chdir_ret = chdir(home_dir.c_str());
+    //     return chdir_ret;
+    // }
+    // else
+    // {
+    //     _args.at(0) = cmd.substr(2, std::string::npos);
+    //     while (cmd.at(0) == ' ')
+    //     {
+    //         cmd.erase(0, 1);
+    //     }
+    //     int chdir_ret = chdir(cmd.c_str());
+    //     return chdir_ret;
+    // }
+}
 
-int minsh_quit(std::vector<std::string> _args);
+int minsh_quit(std::vector<std::string> _args){
+    std::cout << "quit MinSH." << std::endl;
+    exit(0);
+}
 
-int minsh_history(std::vector<std::string> _args);
-
-
-/*
-void show_buildin_cmd()
-{
-    for (auto &i : buildin_cmd)
-    {
-        std::cout << i.first << std::endl;
+int minsh_history(std::vector<std::string> _args){
+    if(_args.size() > 1){
+        std::cout << "too many arguments" << std::endl;
+        return 0;
+    }
+    auto s =_args.at(0);
+    int history_num = stoi(s);
+    for(int i = 0; i <= history_num; i++){
+        MinSH::get_history(i);
     }
 }
 
+
+/*
 int p_cd_function(std::string cmd)
 {
     if (cmd == "cd")
